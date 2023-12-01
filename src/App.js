2a3e3d478cb35b655a8f6fa242e6e5fece250d5c
krishnachandhu.b <krishnachandhu.b@IXEDMACPRO031.local>
2023-12-01 23:17:54 +0530

    linkedin clone app initiated

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>lets build linkedin clone</h1>
+      {/* header */}
+      {/* sidebar */}
+      {/* feed  */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
